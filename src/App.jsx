4aca454e7638c84644068a675cecb94823466a78
Symon Ogwe <symonogwe@gmail.com>
2023-12-31 17:50:48 +0300

    Create data object & store it in state

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-// import { useState } from "react";
+import { useState } from "react";
 
 import "./App.css";
 import Information from "./Information";
 import Cv from "./Cv";
+import cvInformation from "./data";
 
 function App() {
+  const [data, setData] = useState(cvInformation);
   return (
     <>
       <Information />
-      <Cv />
+      <Cv data={data} />
     </>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,19 @@
+const cvInformation = {
+  generalInfo: {
+    name: "",
+    email: "",
+    phone: "",
+  },
+  educationalInfo: {
+    school: "",
+    title: "",
+    dateCompleted: "",
+  },
+  workInfo: {
+    company: "",
+    position: "",
+    responsibility: "",
+  },
+};
+
+export default cvInformation;
