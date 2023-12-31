3bc4bce0b2c129c3e8677772a68c83d5998885d9
Symon Ogwe <symonogwe@gmail.com>
2023-12-31 17:53:39 +0300

    Cv component uses data stored in state to render

--- src/Cv.jsx
+++ src/Cv.jsx
@@ -1,7 +1,20 @@
-export default function Cv() {
+export default function Cv({ data }) {
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>General Information</h1>
+      <p>Name: {data.generalInfo.name}</p>
+      <p>Email: {data.generalInfo.email}</p>
+      <p>Phone: {data.generalInfo.phone}</p>
+
+      <h1>Educational Information</h1>
+      <p>School: {data.educationalInfo.school}</p>
+      <p>Title: {data.educationalInfo.title}</p>
+      <p>Completed: {data.educationalInfo.dateCompleted}</p>
+
+      <h1>Work Information</h1>
+      <p>Company: {data.workInfo.company}</p>
+      <p>Position: {data.workInfo.position}</p>
+      <p>Responsibility: {data.workInfo.responsibility}</p>
     </>
   );
 }
