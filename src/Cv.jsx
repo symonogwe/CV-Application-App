ce6ae2090d498b6df5fb4ae0ad8bcc04fb7a6005
Symon Ogwe <symonogwe@gmail.com>
2024-01-01 07:10:55 +0300

    Cv.jsx returns div.cv-container

--- src/Cv.jsx
+++ src/Cv.jsx
@@ -1,21 +1,21 @@
 export default function Cv({ data }) {
   return (
-    <>
+    <div className="cv-container">
       <h1>MY CV</h1>
-      <h1>General Information</h1>
+      <h2>General Information</h2>
       <p>Name: {data.generalInfo.name}</p>
       <p>Email: {data.generalInfo.email}</p>
       <p>Phone: {data.generalInfo.phone}</p>
 
-      <h1>Educational Information</h1>
+      <h2>Educational Information</h2>
       <p>School: {data.educationalInfo.school}</p>
       <p>Title: {data.educationalInfo.title}</p>
       <p>Completed: {data.educationalInfo.dateCompleted}</p>
 
-      <h1>Work Information</h1>
+      <h2>Work Information</h2>
       <p>Company: {data.workInfo.company}</p>
       <p>Position: {data.workInfo.position}</p>
       <p>Responsibility: {data.workInfo.responsibility}</p>
-    </>
+    </div>
   );
 }
