1be90eee622c4d2a85751423f18f3e9dba5b643c
Symon Ogwe <symonogwe@gmail.com>
2024-01-01 07:07:54 +0300

    Return .info-container div from Information.jsx

--- src/Information.jsx
+++ src/Information.jsx
@@ -2,7 +2,7 @@ import SectionGeneral from "./SectionGeneral";
 
 function Information({ data, setData }) {
   return (
-    <>
+    <div className="info-container">
       <SectionGeneral title={"General Info"} data={data} setData={setData} />
       <SectionGeneral
         title={"Educational Experience"}
@@ -10,7 +10,7 @@ function Information({ data, setData }) {
         setData={setData}
       />
       <SectionGeneral title={"Work Experience"} data={data} setData={setData} />
-    </>
+    </div>
   );
 }
 
