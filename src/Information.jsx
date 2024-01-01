eb5681a6397d47c0bf2e23a995106b38f430e6e9
Symon Ogwe <symonogwe@gmail.com>
2024-01-01 05:44:55 +0300

    Add data & setData to all SectionGeneral component

--- src/Information.jsx
+++ src/Information.jsx
@@ -5,8 +5,12 @@ function Information({ data, setData }) {
   return (
     <>
       <SectionGeneral title={"General Info"} data={data} setData={setData} />
-      <SectionGeneral title={"Educational Experience"} data={data} />
-      <SectionGeneral title={"Work Experience"} data={data} />
+      <SectionGeneral
+        title={"Educational Experience"}
+        data={data}
+        setData={setData}
+      />
+      <SectionGeneral title={"Work Experience"} data={data} setData={setData} />
       <Button />
     </>
   );
