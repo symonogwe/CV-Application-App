d0a11ed1d1b3e025641e0b489e6622576de86029
Symon Ogwe <symonogwe@gmail.com>
2024-01-02 10:24:22 +0300

    EducationalInfo array renders education section
    - Each object in the educationalInfo array inside the data object is
    used to render the Educational Experience section in the cv

--- src/Cv.jsx
+++ src/Cv.jsx
@@ -7,10 +7,16 @@ export default function Cv({ data }) {
       <p>Email: {data.generalInfo.email}</p>
       <p>Phone: {data.generalInfo.phone}</p>
 
-      <h2>Educational Information</h2>
-      <p>School: {data.educationalInfo.school}</p>
-      <p>Title: {data.educationalInfo.title}</p>
-      <p>Completed: {data.educationalInfo.dateCompleted}</p>
+      <div className="cv-education-container">
+        {data.educationalInfo.map((item) => (
+          <div key={item.id} className="cv-education">
+            <h4>School Information {item.id + 1}</h4>
+            <p>School: {item.school}</p>
+            <p>Title: {item.title}</p>
+            <p>Completed: {item.dateCompleted}</p>
+          </div>
+        ))}
+      </div>
 
       <h2>Work Information</h2>
       <p>Company: {data.workInfo.company}</p>
